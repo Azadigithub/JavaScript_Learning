54b46441d63feb63e2b581e8421a556878137c31
Ali <aliazadigithub@gmail.com>
2025-11-22 16:12:30 +0330

    Introduction DOM Elements

---
+++ Scripts/Script.js
@@ -0,0 +1,11 @@
+// Dom => Document Object Model
+
+let element = document.getElementById(""); // ID    => Select UnicElement
+document.getElementsByClassName(""); // Class => Select MultiElements
+element[1];
+
+document.querySelector("Element"); // h1 ,div ,span , ...
+document.querySelector(".ClassnameElement");
+document.querySelector("#ElementID");
+document.querySelectorAll("");
+
