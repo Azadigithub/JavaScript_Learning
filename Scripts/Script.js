994f943303852fa914448f639135e281390db947
Ali <aliazadigithub@gmail.com>
2025-11-23 09:36:19 +0330

    Dom styles , attributes , ClassNames , contents and values

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -6,6 +6,28 @@ element[1];
 
 document.querySelector("Element"); // h1 ,div ,span , ...
 document.querySelector(".ClassnameElement");
-document.querySelector("#ElementID");
+document.querySelector("#ElementID")
 document.querySelectorAll("");
+
+// Dom Css Styles 
+element.style.backgroundColor = 'red'
+
+// Dom Attributes
+element.getAttribute('class')
+element.setAttribute('class' ,'li-elems' )
+element.removeAttribute('class')
+
+// Dom ClassNames
+element.classList.add('new class name')
+element.classList.remove('remove class name')
+element.classList.toggle('add or remove class name')
+
+// Dom Content 
+element.innerHTML   = 'insert a text'
+element.innerText   = 'insert a text'
+element.textContent = 'insert a text'
+
+// Dom Value 
+element.value = 'insert a value'
+
 
